43b37289873d99e963af00e96ed2a007fbfd0fed
ranganath <Ranganath Bhoomannavar>
2026-09-06 17:22:45 +0530

    chore: update application version message to v2.0.0 in server response

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send({message: "application is running v1.0.0"});
+  res.send({message: "application is running v2.0.0"});
 });
 
 app.listen(PORT, () => {
