b578f55e3110e2d3b109e9f0e88a1bf1a9c0cb34
ranganath <Ranganath Bhoomannavar>
2026-09-06 12:42:21 +0530

    chore: initialize package.json with express dependency and basic scripts

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send({message: "application is running v1.0.0"});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
