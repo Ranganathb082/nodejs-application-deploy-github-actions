30db782fb78c13f65eec76d3cdd06466e713e3ae
ranganath <Ranganath Bhoomannavar>
2026-09-06 17:33:53 +0530

    fix: update application version message to v2.0.1 in server response

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send({message: "application is running v2.0.0"});
+  res.send({message: "application is running v2.0.1"});
 });
 
 app.listen(PORT, () => {
